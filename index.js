3bdd1f4b59b8b1dfb3c6bc06d0b23236872c109a
reagan <reaganm746@gmail.com>
2023-10-07 15:43:11 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/characters/583")
+  .then((resp) => resp.json())
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
 }
+then(data => {
+  
+  renderBooks(data);
+})
+.catch(error => {
+  console.error('There was a problem with the fetch operation:', error);
+});
+
+document.addEventListener('DOMContentLoaded', fetchBooks);
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
